7c42a97e680eee64d65808fb1ce5d11ec5a1c38a
lgh <lcliguohao@163.com>
2015-04-16 10:09:41 +0800

    lab4:migrate codes from lab3

--- labcodes/lab4/kern/trap/trap.c
+++ labcodes/lab4/kern/trap/trap.c
@@ -13,6 +13,8 @@
 
 #define TICK_NUM 100
 
+int TICK_COUNT = 0;
+
 static void print_ticks() {
     cprintf("%d ticks\n",TICK_NUM);
 #ifdef DEBUG_GRADE
@@ -48,6 +50,12 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++) {
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+    }
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -186,6 +194,13 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+		 TICK_COUNT++;
+		 if (TICK_COUNT == TICK_NUM)
+		 {
+			  print_ticks();
+			  TICK_COUNT = 0;
+		 }
+
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
